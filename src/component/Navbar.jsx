ab5319c5ad85abb200804b8be02efc0c91308ac5
shanbaho <usmanbahoo381@gmail.com>
2025-08-02 15:29:58 +0500

    Refactor Navbar component for improved functionality and maintainability

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
           <nav className="border-gray-200 dark:bg-gray-900">
             <div className="flex flex-wrap items-center justify-between">
               <a className="flex items-center space-x-3 rtl:space-x-reverse">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap text-sky-600">Mern Stack Developer</span>
+                <span className="self-center text-4xl mt-1 font-semibold whitespace-nowrap text-sky-600">Mern Stack Developer</span>
               </a>
 
               {/* Toggle Button */}
@@ -34,7 +34,7 @@ function Navbar() {
                     <li key={section}>
                       <a
                         href={`#${section}`}
-                        className="block py-2 px-3 text-sky-600 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        className="block py-2 px-3 text-2xl text-sky-600 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                       >
                         {section.charAt(0).toUpperCase() + section.slice(1)}
                       </a>
