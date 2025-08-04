7adf2131d5544b8ce31044d419870c412a5801a2
shanbaho <usmanbahoo381@gmail.com>
2025-08-04 09:25:47 +0500

    Remove Ubuntu icon from Skill component for updated skills representation

--- src/page/Skill.jsx
+++ src/page/Skill.jsx
@@ -61,7 +61,7 @@ function Skill() {
               ["Impress.png", "Impress"],
               ["mongodbCompass.png", "Mongodb Compass"],
               ["postman.png", "Postman"],
-              ["ubuntu.png", "Ubuntu"],
+              
             ].map(([src, title]) => (
               <div
                 className="sm:w-1/4 md:w-1/6 w-1/3 lg:w-1/12 flex flex-col items-center"
